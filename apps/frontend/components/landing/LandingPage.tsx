ec75ef1b6a74915d523dad90f6deb4dd24d6be70
Razin <razin@codecolonies.com>
2026-07-17 17:01:02 +0530

    chore(frontend): add infosection

--- apps/frontend/components/landing/LandingPage.tsx
+++ apps/frontend/components/landing/LandingPage.tsx
@@ -2,12 +2,14 @@ import { CTASection } from '@/components/landing/sections/CTASection'
 import { FeaturesSection } from '@/components/landing/sections/FeaturesSection'
 import { HeroSection } from '@/components/landing/sections/HeroSection'
 import { PricingSection } from '@/components/landing/sections/PricingSection'
+import { InfoSection } from '@/components/landing/sections/InfoSection'
 
 export function LandingPage() {
   return (
     <main className="flex flex-1 flex-col">
       <HeroSection />
       <FeaturesSection />
+      <InfoSection />
       <PricingSection />
       <CTASection />
     </main>
