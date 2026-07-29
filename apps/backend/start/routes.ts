0e42431db5d651efe30f54e3e5333a5a6f0c9fff
Razin <razin@codecolonies.com>
2026-07-30 00:06:56 +0530

    chore(backend): ran migration, fix route typo

--- apps/backend/start/routes.ts
+++ apps/backend/start/routes.ts
@@ -147,12 +147,7 @@ router
     router
       .patch('/subscriptions/:id/delete', [SuperAdminSubscriptionsController, 'softDelete'])
       .use(middleware.requirePermission({ permission: 'platform:tenants_billing' }))
-    // router
-    //   .patch('/organizations/:id', [SuperAdminOrganizationsController, 'update'])
-    //   .use(middleware.requirePermission({ permission: 'platform:tenants_update' }))
-    // router
-    //   .patch('/organizations/:id/soft-delete', [SuperAdminOrganizationsController, 'softDelete'])
-    //   .use(middleware.requirePermission({ permission: 'platform:tenants_delete' }))
+    router
       .patch('/organizations/:id', [SuperAdminOrganizationsController, 'update'])
       .use(middleware.requirePermission({ permission: 'platform:tenants_update' }))
     router
